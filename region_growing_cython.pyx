06ae4a609d04c455bbe6d4214e8eeb9975d9ecf5
iver56 <iver56@hotmail.com>
2019-06-10 19:52:11 +0200

    Add cython equivalent of region growing

---
+++ region_growing_cython.pyx
@@ -0,0 +1,74 @@
+from collections import deque
+
+import numpy as np
+
+
+
+def grow_region_cython(img, seeds, threshold=65, up=True, down=True):
+    """
+    Find a region of connected pixels. The pixel values are assumed to be between 0 and 255
+    https://en.wikipedia.org/wiki/Region_growing
+    :param img: 2D numpy array
+    :param seeds: list of (x, y) tuples
+    :param threshold: the maximum difference between the seed and the grown pixels
+    :param up: direction to consider when checking for neighbours
+    :param down: direction to consider when checking for neighbours
+    :return:
+    """
+    height = len(img)
+    width = len(img[0])
+
+    # Initialize 2D matrix
+    grown = np.zeros((height, width), dtype=np.uint8)
+
+    for pixel_position in seeds:
+        x = pixel_position[0]
+        y = pixel_position[1]
+        grown[y, x] = 255
+
+    for seed in seeds:
+        queue = deque([seed])
+
+        x = seed[0]
+        y = seed[1]
+        seed_intensity = img[y, x]
+        seed_threshold = max(0, seed_intensity - threshold)
+        while queue:
+            pixel_position = queue.popleft()
+            neighbours = get_neighbours(pixel_position, width, height, grown, up, down)
+            for neighbour in neighbours:
+                neighbour_x = neighbour[0]
+                neighbour_y = neighbour[1]
+                neighbour_intensity = img[neighbour_y, neighbour_x]
+                if neighbour_intensity >= seed_threshold:
+                    grown[neighbour_y, neighbour_x] = 255
+                    queue.append(neighbour)
+
+    return grown
+
+
+def get_neighbours(position, width, height, grown, up, down):
+    """
+    Return connected neighbours that are not already "grown" (i.e. added to the region)
+    """
+    x = position[0]
+    y = position[1]
+    neighbours = []
+
+    # Left
+    if x > 0 and not grown[y, x - 1]:
+        neighbours.append((x - 1, y))
+
+    # Up
+    if up and y > 0 and not grown[y - 1, x]:
+        neighbours.append((x, y - 1))
+
+    # Right
+    if x < width - 1 and not grown[y, x + 1]:
+        neighbours.append((x + 1, y))
+
+    # Down
+    if down and y < height - 1 and not grown[y + 1, x]:
+        neighbours.append((x, y + 1))
+
+    return neighbours
--- run_region_growing.py
+++ run_region_growing.py
@@ -1,16 +1,49 @@
-from PIL import Image
 import numpy as np
+import pyximport
+
+pyximport.install()
+from PIL import Image
 
 from region_growing import grow_region
+from region_growing_cython import grow_region_cython
 from timer import timer
+
 
 if __name__ == "__main__":
     image = Image.open("example.png")
     image_np = np.array(image)
     seeds = [(200, 200)]
 
-    with timer('region growing'):
-        for _ in range(8):
-            grown_image = grow_region(image_np, seeds, threshold=0)
+    with timer("warmup"):
+        grown_image_up = grow_region(image_np, seeds, threshold=0, up=True, down=False)
+        grown_image_down = grow_region(
+            image_np, seeds, threshold=0, up=False, down=True
+        )
 
-    Image.fromarray(grown_image).save("example_grown.png")
+    with timer("region growing"):
+        for _ in range(4):
+            grown_image_up = grow_region(
+                image_np, seeds, threshold=0, up=True, down=False
+            )
+            grown_image_down = grow_region(
+                image_np, seeds, threshold=0, up=False, down=True
+            )
+
+    with timer("warmup"):
+        grown_image_up = grow_region_cython(
+            image_np, seeds, threshold=0, up=True, down=False
+        )
+        grown_image_down = grow_region_cython(
+            image_np, seeds, threshold=0, up=False, down=True
+        )
+
+    with timer("region growing cython"):
+        for _ in range(4):
+            grown_image_up = grow_region_cython(
+                image_np, seeds, threshold=0, up=True, down=False
+            )
+            grown_image_down = grow_region_cython(
+                image_np, seeds, threshold=0, up=False, down=True
+            )
+
+    Image.fromarray(grown_image_down).save("example_grown.png")
